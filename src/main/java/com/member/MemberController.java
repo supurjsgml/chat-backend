4244d7040d54b6fbee55c5c9acf7a3b582e0c4c9
P212236 <P212236@SKMNS-P212236.skmns.co.kr>
2021-03-18 14:49:55 +0900

    첫 커밋

---
+++ src/main/java/com/member/MemberController.java
@@ -0,0 +1,30 @@
+package com.member;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.naming.spi.DirStateFactory.Result;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.member.service.MemberService;
+
+
+@RestController
+@RequestMapping("/")
+public class MemberController {
+	
+	@Autowired
+	private MemberService memberService;
+
+	@GetMapping("/member")
+	public ResponseEntity<List<Map<String, Object>>> getSample() {
+		Result resulet = new Result(memberService.getSample(), null);
+		return new ResponseEntity(resulet, HttpStatus.OK);
+	}
+}
