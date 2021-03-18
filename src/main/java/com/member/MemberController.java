27b53d696c4635fc53afef3eee400c8c22512cbb
P212236 <P212236@SKMNS-P212236.skmns.co.kr>
2021-03-18 16:40:07 +0900

    포포포로호호호

--- src/main/java/com/member/MemberController.java
+++ src/main/java/com/member/MemberController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +25,12 @@ public class MemberController {
 
 	@GetMapping("/member")
 	public ResponseEntity<List<Map<String, Object>>> getSample() {
+		Result resulet = new Result(memberService.getSample(), null);
+		return new ResponseEntity(resulet, HttpStatus.OK);
+	}
+	
+	@PostMapping(value = "/member", produces = "application/json")
+	public ResponseEntity<List<Map<String, Object>>> PostSample() {
 		Result resulet = new Result(memberService.getSample(), null);
 		return new ResponseEntity(resulet, HttpStatus.OK);
 	}
